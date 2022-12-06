775e8163a059067a238b704a1ce1e4d1ef403e4f
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-12-06 07:27:14 -0500

    Almost complete a couple Tweaks Left

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,39 +1,43 @@
 'use strict'
 const userName= '';
 const userQuestion = '';
+const outcomeBtn = document.getElementById('outcomeBtn');
+const outcome = document.getElementById('outcome');
 let eightBall = '';
 
-let randomNumber = Math.floor(Math.random()* 8);
 
-userName ? console.log(`Hello ${userName}`):console.log(`Hello`);
 
-console.log(userName + userQuestion);
 
-switch(randomNumber){
-    case 0:
-        eightBall = 'It is certain';
-        break;
-    case 1:
-        eightBall = 'You my sources say your crazy';
-        break;
-    case 2:
-        eightBall = 'Outlook is as good a Biden remembering his name';
-        break;
-    case 3:
-        eightBall = `Reply is as hazy as Biden's memory, try again`;
-        break;
-    case 4:
-        eightBall = 'My sources say NO!';
-        break;
-    case 5:
-        eightBall = 'I cannot say at this time';
-        break;
-    case 6:
-        eightBall = `Why are you asking me I don't know`;
-        break;
-    case 7:
-        eightBall = 'Signs point to yes and Desantis 2024';
-        break;
-}
-
-console.log(eightBall);
+//userName ? console.log(`Hello ${userName}`):console.log(`Hello`);
+//console.log(userName + userQuestion);
+outcomeBtn.addEventListener('click', function(){
+    let randomNumber = Math.floor(Math.random()* 8);
+    switch(randomNumber){
+        case 0:
+            eightBall = 'It is certain';
+            break;
+        case 1:
+            eightBall = 'You my sources say your crazy';
+            break;
+        case 2:
+            eightBall = 'Outlook is as good a Biden remembering his name';
+            break;
+        case 3:
+            eightBall = `Reply is as hazy as Biden's memory, try again`;
+            break;
+        case 4:
+            eightBall = 'My sources say NO!';
+            break;
+        case 5:
+            eightBall = 'I cannot say at this time';
+            break;
+        case 6:
+            eightBall = `Why are you asking me I don't know`;
+            break;
+        case 7:
+            eightBall = 'Signs point to yes and Desantis 2024';
+            break;
+    }
+    outcome.innerText=eightBall;
+    console.log(eightBall);
+});
